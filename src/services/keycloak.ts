5eff4bbb2e7c8ca3ce11c3ae6bf53bbc219e59fd
juarez <d.juarez@dkfz-heidelberg.de>
2023-12-14 21:10:30 +0100

    Added: Keycloak (II)

--- src/services/keycloak.ts
+++ src/services/keycloak.ts
@@ -27,15 +27,3 @@ const KeyCloakService = {
 };
 
 export default KeyCloakService;
-
-/*
-import Keycloak from 'keycloak-js';
-
-const keycloak = Keycloak({
-    url: process.env.VUE_APP_KEYCLOAK_URL,
-    realm: process.env.VUE_APP_KEYCLOAK_REALM,
-    clientId: process.env.VUE_APP_KEYCLOAK_CLIENT_ID,
-});
-
-export default keycloak;
-*/

1970e53e49aae2f37c200a0c92dc10d6475f92bd
juarez <d.juarez@dkfz-heidelberg.de>
2023-12-19 12:15:28 +0100

    Added: Environment Variables

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,8 +4,9 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
-## [1.1.0 - 2023-12-14]
+## [1.1.0 - 2023-12-19]
 ### Added
 - Dockerfile
 - Single Spa
 - Keycloak
+- Environment Variables
--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,23 @@
-/*
-const { defineConfig } = require('@vue/cli-service')
+const dotenv = require('dotenv').config();
 
-module.exports = defineConfig({
-  transpileDependencies: true
-})
-*/
 module.exports = {
-  configureWebpack: {
-    output: {
-      libraryTarget: "system",
+    configureWebpack: {
+        output: {
+            libraryTarget: "system",
+        },
     },
-  },
-  chainWebpack: (config) => {
-    if (config.plugins.has("SystemJSPublicPathWebpackPlugin")) {
-      config.plugins.delete("SystemJSPublicPathWebpackPlugin");
-    }
-  },
-  filenameHashing: false,
+    chainWebpack: (config) => {
+        if (config.plugins.has("SystemJSPublicPathWebpackPlugin")) {
+            config.plugins.delete("SystemJSPublicPathWebpackPlugin");
+        }
+        config.plugin('define').tap((args) => {
+            const env = dotenv.parsed;
+            args[0]['process.env'] = {
+                ...args[0]['process.env'],
+                ...env,
+            };
+            return args;
+        });
+    },
+    filenameHashing: false,
 };

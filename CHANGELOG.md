e56317e7509ce945f25b1d6f4f159e834e1ea213
juarez <d.juarez@dkfz-heidelberg.de>
2023-12-31 13:06:23 +0100

    Added: Project Manager Backend Service

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,9 +4,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
-## [1.1.0 - 2023-12-19]
+## [1.1.0 - 2023-12-31]
 ### Added
 - Dockerfile
 - Single Spa
 - Keycloak
 - Environment Variables
+- Project Manager Backend Service
--- src/components/mainDashboard.vue
+++ src/components/mainDashboard.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="container mt-4">
       <h1>Main Dashboard</h1>
-<br/>
+      <br/>
       <div class="row">
         <div class="col-md-8">
           <h2>Requests</h2>
@@ -41,7 +41,7 @@
       </table>
     </div>
 
-    <br /><br /><br />
+    <br/><br/><br/>
 
     <div class="container mt-4">
       <h2>Notifications</h2>
@@ -50,7 +50,7 @@
         <thead>
         <tr>
           <th scope="col">Project ID</th>
-          <th scope="col">PRN</th>
+          <th scope="col">DRN</th>
           <th scope="col">User</th>
           <th scope="col">Date</th>
           <th scope="col">Notification</th>
@@ -61,7 +61,7 @@
         <!-- Add rows with dummy data -->
         <tr v-for="(item, index) in secondTableData" :key="index">
           <td>{{ item.projectId }}</td>
-          <td>{{ item.prn }}</td>
+          <td>{{ item.drn }}</td>
           <td>{{ item.user }}</td>
           <td>{{ item.date }}</td>
           <td>{{ item.notification }}</td>
@@ -73,30 +73,36 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import {defineComponent} from 'vue';
+import {ProjectManagerContext, ProjetManagerBackendService, Site} from "@/services/projetManagerBackendService";
 
 export default defineComponent({
   data() {
     return {
+      site: Site.PROJECT_DASHBOARD_SITE,
+      context: undefined as ProjectManagerContext | undefined,
+      projectManagerBackendService: undefined as ProjetManagerBackendService | undefined,
       tableData: [] as { title: string; projectId: string; drn: string; date: string; status: string }[],
       secondTableData: [
-        { projectId: 'PR001', prn: '12345', user: 'User1', date: '2023-04-15', notification: 'Notification 1' },
-        { projectId: 'PR002', prn: '67890', user: 'User2', date: '2023-05-20', notification: 'Notification 2' },
+        {projectId: 'PR001', drn: '12345', user: 'User1', date: '2023-04-15', notification: 'Notification 1'},
+        {projectId: 'PR002', drn: '67890', user: 'User2', date: '2023-05-20', notification: 'Notification 2'},
       ],
     };
   },
   mounted() {
     this.loadProjectData();
+    this.context = new ProjectManagerContext()
+    this.projectManagerBackendService = new ProjetManagerBackendService(this.context, this.site)
   },
 
   methods: {
     async loadProjectData(): Promise<void> {
       try {
         const response = await fetch('/projects.json');
-        const { projects } = await response.json();
+        const {projects} = await response.json();
 
         this.tableData = projects;
-        console.log("table"+this.tableData);
+        console.log("table" + this.tableData);
       } catch (error) {
         console.error('Error loading project data:', error);
       }

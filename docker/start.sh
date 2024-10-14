aaf61e80feaa792721c084ef699c0d68702c5c49
djuarezgf <d.juarez@dkfz-heidelberg.de>
2024-10-14 11:09:15 +0200

    Added: Test

---
+++ docker/start.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+envsubst < /usr/share/nginx/html/app.js > /usr/share/nginx/html/app.temp.js
+mv /usr/share/nginx/html/app.temp.js /usr/share/nginx/html/app.js
+#envsubst '${TEILER_DASHBOARD_SERVER_NAME} ${TEILER_ORCHESTRATOR_URL}'< /etc/nginx/nginx.template.conf > /etc/nginx/nginx.conf
+
+echo 'Start Teiler Dashboard in NGINX in foreground (non-daemon-mode)'
+exec nginx -g 'daemon off;'

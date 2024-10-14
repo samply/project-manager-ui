45a0b6456ed853b788c30169531df0c0556fa363
djuarezgf <d.juarez@dkfz-heidelberg.de>
2024-10-14 11:32:40 +0200

    Test

--- docker/start.sh
+++ docker/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-envsubst < /usr/share/nginx/html/app.js > /usr/share/nginx/html/app.temp.js
+envsubst < /usr/share/nginx/html/js/app.js > /usr/share/nginx/html/js/app.temp.js
 mv /usr/share/nginx/html/app.temp.js /usr/share/nginx/html/app.js
 #envsubst '${TEILER_DASHBOARD_SERVER_NAME} ${TEILER_ORCHESTRATOR_URL}'< /etc/nginx/nginx.template.conf > /etc/nginx/nginx.conf
 

fbe44c1d818cfe6ef2dcf4e2eec2a6a8c7a57e50
Thomas Köhler <thomas.koehler@dkfz-heidelberg.de>
2025-01-20 10:24:07 +0100

    - add paging for Notifications

--- src/components/Notification.vue
+++ src/components/Notification.vue
@@ -23,6 +23,16 @@ export default class NotificationBox extends Vue {
   @Prop() readonly showInPanel!: boolean;
   @Prop({ type: Function, required: true }) readonly callUpdateNotifications!: () => void;
   @Prop({ type: Function, required: true }) readonly callToggleNotification!: () => void;
+  currentPage = 1;
+  totalPages = 1;
+  notificationsPerPage = 8;
+  pagedNotifications: Notification[] = []
+
+  @Watch("notifications", {immediate: true, deep: true})
+  onNotificationsChange(newValue: string[], oldValue: string[]) {
+    this.totalPages = Math.round(newValue.length / this.notificationsPerPage);
+    this.pagingNotifications();
+  }
 
   convertDate(date: Date) {
     return format(date, 'yyyy-MM-dd HH:mm:ss')
@@ -32,9 +42,16 @@ export default class NotificationBox extends Vue {
     const params = new Map<string, string>;
     params.set('notification-id', '' + notificationId)
     this.projectManagerBackendService.fetchData(Module.NOTIFICATIONS_MODULE, Action.SET_NOTIFICATION_AS_READ_ACTION, this.context, params)
+    this.pagingNotifications()
     this.callUpdateNotifications()
   }
-
+  pagingNotifications() {
+    this.pagedNotifications = this.notifications.slice((this.currentPage - 1) * this.notificationsPerPage, ((this.currentPage - 1) * this.notificationsPerPage) + this.notificationsPerPage)
+  }
+  firstPage() {this.currentPage = 1;this.pagingNotifications()}
+  previousPage() {if (this.currentPage > 1) {this.currentPage--} this.pagingNotifications()}
+  nextPage() {if (this.currentPage < this.totalPages) {this.currentPage++} this.pagingNotifications()}
+  lastPage() {this.currentPage = this.totalPages; this.pagingNotifications()}
 }
 </script>
 
@@ -47,7 +64,7 @@ export default class NotificationBox extends Vue {
       </button>
     </div>
     <div class="notification-box">
-      <div v-for="(notification,index) in notifications" :key="index" class="card mb-3">
+      <div v-for="(notification,index) in pagedNotifications" :key="index" class="card mb-3">
         <!--<div class="card-body" :class="{ 'expanded': true }">-->
         <div class="card-body" v-if="!notification.read">
           <div style="display:flex; flex-flow: row; justify-content: space-between">
@@ -75,6 +92,21 @@ export default class NotificationBox extends Vue {
                       </div>-->
         </div>
       </div>
+    </div>
+    <div v-if="totalPages > 1" class="pager">
+      <button @click="firstPage" class="btn btn-primary">
+        <i class="bi bi-skip-start-fill" style="font-size: medium"></i>
+      </button>
+      <button @click="previousPage" class="btn btn-primary" style="rotate: 180deg">
+        <i class="bi bi-play-fill" style="font-size: medium"></i>
+      </button>
+      <span>{{currentPage}} / {{totalPages}}</span>
+      <button @click="nextPage" class="btn btn-primary">
+        <i class="bi bi-play-fill" style="font-size: medium"></i>
+      </button>
+      <button @click="lastPage" class="btn btn-primary">
+        <i class="bi bi-skip-end-fill" style="font-size: medium"></i>
+      </button>
     </div>
   </div>
 </template>
@@ -143,5 +175,25 @@ export default class NotificationBox extends Vue {
 .btn-close {
   width: 0.5em;
   height: 0.5em;
+}
+.pager {
+  display: flex;
+  justify-content: end;
+  padding: 2%;
+}
+.pager span {
+  display: flex;
+  border: 1px solid #cccccc;
+  border-radius: 5px;
+  padding: 0 10px 1px 10px;
+  background-color: white;
+}
+.pager button {
+  padding-top: 3px;
+  padding-bottom: 3px;
+}
+.pager button, .pager span {
+  margin-left: 10px;
+  align-items: center;
 }
 </style>

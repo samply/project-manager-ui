78dc3438d19835ecef051cc59061563e77ee10f4
Thomas Köhler <thomas.koehler@dkfz-heidelberg.de>
2026-05-27 13:44:17 +0200

    chore: fix css bug

--- src/components/ProjectView.vue
+++ src/components/ProjectView.vue
@@ -423,7 +423,7 @@
                           :is-file="false"/>
           </div>
         </div>
-        <div class="documents data-container mt-12" v-if="currentMenuStep==='Documents'">
+        <div class="documents data-container mt-12" v-if="currentMenuStep==='Documents'" style="padding: 0 4% 0 5%">
           <div class="box-header"><span>Documents</span></div>
           <div style="padding: 2%">
             <DownloadFormTemplatePdfButtons :form-templates="formTemplates" :context="context"
@@ -1947,7 +1947,6 @@ export default defineComponent({
   box-shadow: 0 2px 1px -1px rgba(0, 0, 0, 0.2),
   0 1px 1px 0 rgba(0, 0, 0, 0.14),
   0 1px 3px 0 rgba(0, 0, 0, 0.12);*/
-  padding: 0 4% 0 5%;
   margin-bottom: 1.5%;
 }
 
